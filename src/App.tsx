b29eec06d5f4bb171dfcf0d4334058f167fc1ee0
sobhisobhi <sobhi.boughanmi@gmail.com>
2022-08-03 11:28:40 +0200

    second commit: add Chart fig with fake data

--- src/App.tsx
+++ src/App.tsx
@@ -1,125 +1,89 @@
-import './App.css';
-import react, { useState, useEffect } from 'react';
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
 
-const App = () => { 
-  
-  const [graph, setGraph] = useState<any>({
-    label: [],
-    data: []
-  });
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-  const [secondGraph, setSecondGraph] = useState<any>({
-    data: []
-  });
-  
-  const firstLineGraphData = [
-    {
-      value: 5
+export const options = {
+  responsive: true,
+  interaction: {
+    mode: 'index' as const,
+    intersect: false,
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart - Multi Axis',
     },
-    {
-      value: 10
+  },
+  scales: {
+    y: {
+      type: 'linear' as const,
+      display: true,
+      position: 'left' as const,
     },
-    {
-      value: 8
-    }
-  ];
-
-  const secondLineGraphData = [
-    {
-      value: 7
-    },
-    {
-      value: 2
-    },
-    {
-      value: 9
-    }
-  ];
-
-  useEffect(() => {
-
-    const data: any[] = [];
-    const label: any[] = [];
-
-    const second_line_data: any[] = [];
-
-    firstLineGraphData?.map((v:any,index) => {
-      data.push(v?.value);
-      label.push(index)
-    });
-
-    secondLineGraphData?.map((v:any) => {
-      second_line_data.push(v?.value);
-    });
-
-    setGraph({
-      label: label,
-      data: data
-    });
-
-    setSecondGraph({
-      data: second_line_data,
-    });
-
-  },[]);
-  
-  const data = {
-    labels: graph.label,
-    datasets: [
-      {
-        label: 'React',
-        data: graph.data,
-        fill: false,
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgba(255, 99, 132, 0.2)',
-        yAxisID: 'y-axis-1',
+    /* y1: {
+      type: 'linear' as const,
+      display: true,
+      position: 'right' as const,
+      grid: {
+        drawOnChartArea: false,
       },
-      {
-        label: 'Angular',
-        data: secondGraph.data,
-        fill: false,
-        backgroundColor: 'rgb(54, 162, 235)',
-        borderColor: 'rgba(54, 162, 235, 0.2)',
-        yAxisID: 'y-axis-2',
-      },
-    ],
-  };
+    }, */
+  },
+};
 
-  const options: any = {
-    scales: {
-      yAxes: [
-        {
-          type: 'linear',
-          display: true,
-          position: 'left',
-          id: 'y-axis-1',
-        },
-        {
-          type: 'linear',
-          display: true,
-          position: 'right',
-          id: 'y-axis-2',
-          gridLines: {
-            drawOnArea: false,
-          },
-        },
-      ],
+const labels = [ "January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December" ];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'ReactJS',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000e3 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      yAxisID: 'y',
     },
-  };
-  
+    {
+      label: 'VueJS',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000e3 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      yAxisID: 'y',
+    },
+  ],
+};
 
+export function App() {
   return (
-    <>
-      <div className="App">
-      <h3>React Multi Axis Line Chart Example - CodeCheef </h3>
-        <div style={{ height:'500px',width:'500px',margin:'0 auto' }}>
-           <Line data={data} options={options} />
-        </div>
-      </div>
-    </>
-  );
-
+  <>
+    <div style={{ 'width': '50%', 'height': '400px', margin: 'auto'}}>
+      <Line options={options} data={data} />
+    </div>
+    <div>
+    
+    </div>
+  </>
+    );
 }
-
-export default App;
